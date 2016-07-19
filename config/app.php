e5b07b271ac3daf1723f76c04a99b873e23fa5d6
Miguel Mendez <keloxers@gmail.com>
2016-07-19 19:42:38 -0300

    pdf creator

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
 
+        Elibyy\TCPDF\ServiceProvider::class,
+
+
     ],
 
     /*
@@ -207,6 +210,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Elibyy\TCPDF\Facades\TCPDF::class
+
 
     ],
 
